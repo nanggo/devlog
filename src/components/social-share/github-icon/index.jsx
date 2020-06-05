536e1abd317db2975af4b47d9ed705d604ce7e92
nanggo <yamsiri@gmail.com>
2020-06-06 06:19:42 +0900

    Update misc

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/nanggo/devlog"
+      href="https://github.com/nanggo"
       className="github"
       aria-label="GitHub"
     >
