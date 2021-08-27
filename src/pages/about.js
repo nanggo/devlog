8e53d63b64870b664a4916482723013d47c0f2f6
nanggo <yamsiri@gmail.com>
2021-08-27 13:07:40 +0900

    Modify: about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,34 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { graphql } from 'gatsby'
 
+import { Head } from '../components/head'
 import { rhythm } from '../utils/typography'
 import * as Lang from '../constants'
 
+import '../styles/resume.scss'
+
 export default ({ data }) => {
   const resumes = data.allMarkdownRemark.edges
+  const resumeData = data.site.siteMetadata.resume
 
   const resume = resumes
-    .filter(({ node }) => node.frontmatter.lang === Lang.ENGLISH)
+    .filter(({ node }) => node.frontmatter.lang === Lang.KOREAN)
     .map(({ node }) => node)[0]
 
+  useEffect(() => {
+    console.info('test')
+  }, [])
+
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
-          3 / 4
-        )}`,
-      }}
-    >
-      <div dangerouslySetInnerHTML={{ __html: resume.html }} />
-    </div>
+    <>
+      <Head
+        title={resumeData.title}
+        description={resumeData.description}
+        thumbnail={resumeData.thumbnail}
+      />
+      <div
+        className="about"
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
+            3 / 4
+          )}`,
+        }}
+      >
+        <div dangerouslySetInnerHTML={{ __html: resume.html }} />
+      </div>
+    </>
   )
 }
 
 export const pageQuery = graphql`
   query {
+    site {
+      siteMetadata {
+        resume {
+          title
+          description
+          thumbnail
+        }
+      }
+    }
     allMarkdownRemark(filter: { frontmatter: { category: { eq: null } } }) {
       edges {
         node {
